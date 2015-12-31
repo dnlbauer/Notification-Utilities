e27dc19b26fc0c371137d79d0c002ae627f30538
danijoo <danijob88@gmail.com>
2015-12-31 02:40:12 +0100

    created main activity

--- app/src/main/java/net/headlezz/notificationlogger/MainActivity.java
+++ app/src/main/java/net/headlezz/notificationlogger/MainActivity.java
@@ -1,7 +1,36 @@
 package net.headlezz.notificationlogger;
 
-/**
- * Created by Daniel on 31.12.2015.
- */
-public class MainActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        ButterKnife.bind(this);
+
+        mToolbar.inflateMenu(R.menu.main_activity);
+        mToolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if(item.getItemId() == R.id.menu_settings) {
+            Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
 }
